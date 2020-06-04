28873979822f56ba1929356b30b38c07e7aa8aa5
Edy Segura <edysegura@gmail.com>
2020-06-04 00:20:14 -0300

    Add CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
-import express, { Response, Request } from 'express';
+import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
 const PORT = process.env.PORT || 3333;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
