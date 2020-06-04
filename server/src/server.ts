f2e4b31db8d675c935e6f1d7a0fc3c0be02ddabc
Edy Segura <edysegura@gmail.com>
2020-06-03 21:46:52 -0300

    Add static route for images

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express, { Response, Request } from 'express';
+import path from 'path';
 import routes from './routes';
 
 const PORT = process.env.PORT || 3333;
@@ -6,6 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+const uploadsLocation = path.resolve(__dirname, '..', 'uploads');
+app.use('/uploads', express.static(uploadsLocation));
 
 app.listen(PORT, () => {
   console.log(`The server is up and running on http://localhost:${PORT}`);
