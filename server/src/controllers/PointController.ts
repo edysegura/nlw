9b2383809c78aada627514c1052af6be97da0980
Edy Segura <edysegura@gmail.com>
2020-06-04 00:16:35 -0300

    Add HTTP GET points route

--- server/src/controllers/PointController.ts
+++ server/src/controllers/PointController.ts
@@ -3,6 +3,24 @@ import knex from '../database/connection';
 
 export default class PointController {
 
+  static async index(request: Request, response: Response) {
+    const { city, uf, items } = request.query;
+
+    const itemsId = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', itemsId)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return response.json(points);
+  }
+
   static async show(request: Request, response: Response) {
     const { id } = request.params;
 
@@ -40,7 +58,7 @@ export default class PointController {
 
     const trx = await knex.transaction();
 
-    const [ insertedPointId ] = await trx('points').insert({
+    const point = {
       image: 'https://images.unsplash.com/photo-1506617420156-8e4536971650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=600',
       name,
       email,
@@ -49,7 +67,9 @@ export default class PointController {
       longitude,
       city,
       uf
-    });
+    };
+
+    const [ insertedPointId ] = await trx('points').insert(point);
 
     const pointItems = items.map((itemId: number) => {
       return {
@@ -61,7 +81,10 @@ export default class PointController {
     await trx('point_items').insert(pointItems);
     trx.commit();
 
-    return response.json(request.body);
+    return response.json({
+      id: insertedPointId,
+      ... point
+    });
   }
 
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,6 +7,7 @@ const routes = express.Router();
 routes.get('/items', ItemController.index);
 
 routes.post('/points', PointController.create);
+routes.get('/points', PointController.index);
 routes.get('/points/:id', PointController.show);
 
 export default routes;
